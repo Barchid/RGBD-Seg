c428e3ead8ea99fabd88423854adf43f85221b57
Sami Barchid <sami.barchid@univ-lille.fr>
2021-08-17 15:23:19 +0200

    fix bug of eager exec

--- src/confusion_matrix.py
+++ src/confusion_matrix.py
@@ -10,7 +10,7 @@ import numpy as np
 import numbers
 import torch
 import tensorflow as tf
-
+tf.compat.v1.disable_eager_execution()
 
 class ConfusionMatrixTensorflow:
     def __init__(self, n_classes):
