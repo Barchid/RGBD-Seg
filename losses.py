65393db0577dd706854eff53cdd1d3c57a7f9a71
Sami Barchid <sami.barchid@univ-lille.fr>
2021-08-23 12:15:27 +0200

    more losses because I'm a mad man

--- losses.py
+++ losses.py
@@ -21,3 +21,39 @@ class OhemCELoss(nn.Module):
         else:
             loss = loss[:self.n_min]
         return torch.mean(loss)
+
+
+class WeightedOhemCELoss(nn.Module):
+    def __init__(self, thresh, n_min, num_classes, ignore_lb=255, *args, **kwargs):
+        super(WeightedOhemCELoss, self).__init__()
+        self.thresh = -torch.log(torch.tensor(thresh, dtype=torch.float)).cuda()
+        self.n_min = n_min
+        self.ignore_lb = ignore_lb
+        self.num_classes = num_classes
+        # self.criteria = nn.CrossEntropyLoss(ignore_index=ignore_lb, reduction='none')
+
+    def forward(self, logits, labels):
+        N, C, H, W = logits.size()
+        criteria = nn.CrossEntropyLoss(weight=enet_weighing(labels, self.num_classes).cuda(), ignore_index=self.ignore_lb, reduction='none')
+        loss = criteria(logits, labels).view(-1)
+        loss, _ = torch.sort(loss, descending=True)
+        if loss[self.n_min] > self.thresh:
+            loss = loss[loss > self.thresh]
+        else:
+            loss = loss[:self.n_min]
+        return torch.mean(loss)
+
+
+class SoftmaxFocalLoss(nn.Module):
+    def __init__(self, gamma, ignore_lb=255, *args, **kwargs):
+        super(SoftmaxFocalLoss, self).__init__()
+        self.gamma = gamma
+        self.nll = nn.NLLLoss(ignore_index=ignore_lb)
+
+    def forward(self, logits, labels):
+        scores = F.softmax(logits, dim=1)
+        factor = torch.pow(1.-scores, self.gamma)
+        log_score = F.log_softmax(logits, dim=1)
+        log_score = factor * log_score
+        loss = self.nll(log_score, labels)
+        return loss
